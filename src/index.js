a1b115d75ff82b0a13297ef2ee59fbfedbefd81a
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-01-23 22:26:07 +0500

    refactor: update solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-module.exports = function reverse (n) {
-  if (n < 0) n *= -1;
-  return n.toString().split('').reverse().join('');
+module.exports = function reverse(n) {
+  return parseInt(n.toString().split('').reverse().join(''));
 }
